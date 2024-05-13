368840fd2b5eda9365de1f4432deee5d9323129d
User_HOB <a.21308051280514@cbtis128.edu.mx>
2024-05-13 04:25:07 +0000

    Actualizar

---
+++ lib/data/category_model.dart
@@ -0,0 +1,14 @@
+class Category {
+  final int id;
+  final String name;
+
+  Category(this.id, this.name);
+}
+
+List<Category> categories = [
+  Category(0, 'Todos'),
+  Category(1, 'Versa'),
+  Category(2, 'Kicks'),
+  Category(3, 'Urvan'),
+  Category(4, 'Altima'),
+];
---
+++ lib/data/plant_data.dart
@@ -0,0 +1,77 @@
+import 'package:munoz0514/data/plant_model.dart';
+
+List<Plants> plants = [
+  Plants(
+    id: 0,
+    name: 'Versa',
+    imagePath: 'assets/images/image1.png',
+    category: 'Versa',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ',
+    price: 15000.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 1,
+    name: 'Kicks',
+    imagePath: 'assets/images/image2.png',
+    category: 'Kicks',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ',
+    price: 20000.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 2,
+    name: 'Urvan',
+    imagePath: 'assets/images/image3.png',
+    category: 'Urvan',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ',
+    price: 18000.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 3,
+    name: 'Altima',
+    imagePath: 'assets/images/image5.png',
+    category: 'Altima',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ',
+    price: 19000.00,
+    isFavorit: false,
+  ),
+];
+
+List<Plants> populerPlants = [
+  Plants(
+    id: 0,
+    name: 'Urvan',
+    imagePath: 'assets/images/01.png',
+    category: 'Office',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ',
+    price: 18000.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 1,
+    name: 'Kicks',
+    imagePath: 'assets/images/02.png',
+    category: 'Office',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ',
+    price: 20000.00,
+    isFavorit: false,
+  ),
+  Plants(
+    id: 2,
+    name: 'Versa',
+    imagePath: 'assets/images/03.png',
+    category: 'Office',
+    description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ',
+    price: 15000.00,
+    isFavorit: false,
+  ),
+];
